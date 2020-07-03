03a74b38e94a021b0feb4229246a56d41ca2404e
Fred Ojure <fredojure@hotmail.com>
2020-07-03 19:05:08 +0300

    Added process_results method in request.py

--- app/request.py
+++ app/request.py
@@ -11,11 +11,11 @@ api_key = app.config['MOVIE_API_KEY']
 base_url = app.config["MOVIE_API_BASE_URL"]
 
 
-def get_news(category):
+def get_news():
     '''
     Function that gets the json response to our url request
     '''
-    get_news_url = base_url.format(category,api_key)
+    get_news_url = base_url.format()
 
     with urllib.request.urlopen(get_news_url) as url:
         get_news_data = url.read()
@@ -27,5 +27,29 @@ def get_news(category):
             news_results_list = get_news_response['results']
             news_results = process_results(news_results_list)
 
+    return news_results
+
+def process_results(news_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain movie details
+
+    Returns :
+        news_results: A list of movie objects
+    '''
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        name = news_item.get('name')
+        description =news_item.get('description')
+        url = news_item.get('url')
+        category = news_item.get('category')
+        language = news_item.get('language')
+        country = news_item.get('country')
+
+        news_object = News(id,name,description,url,categorye,language, country)
+        news_results.append(news_object)
 
     return news_results
